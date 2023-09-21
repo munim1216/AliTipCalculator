85bbf48d8cc3b6afa9a37329f0fbf9eff8f2d03d
BT_1N3_03 <sample@bths.edu>
2023-09-21 10:03:11 -0400

    Initial Commit, variables created

---
+++ src/TipCalc.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class TipCalc {
+    public static void main(String[] args) {
+        // initializing variables
+        Scanner scan = new Scanner(System.in);
+        int groupSize;
+        double tip;
+        double itemAddedToBill;
+        double billBeforeTip;
+        double billAfterTip;
+
+
+    }
+}

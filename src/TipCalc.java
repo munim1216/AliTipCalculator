eeb84f02c5283572ccba08b30ed269b7f5830da0
BT_1N3_03 <sample@bths.edu>
2023-09-21 10:16:04 -0400

    Number of people in a group, and whether they are paying together or separately has been finished.

--- src/TipCalc.java
+++ src/TipCalc.java
@@ -8,7 +8,19 @@ public class TipCalc {
         double itemAddedToBill;
         double billBeforeTip;
         double billAfterTip;
+        int togetherPay;
+        // ask if paying individually or together
+        // gathering user input
+        System.out.println("Welcome to Jade's tip calculator!");
+        System.out.print("How many people are in your group: ");
+        groupSize = scan.nextInt();
+        scan.nextLine();
+        System.out.print("Type 1 to pay together, Type 0 to pay separately: ");
+        togetherPay = scan.nextInt();
+        while (togetherPay != 1 && togetherPay != 0) {
+            System.out.print("Please type 1 or 0, not any other number: ");
+            togetherPay = scan.nextInt();
 
-
+        }
     }
 }

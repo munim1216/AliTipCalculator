f0df52ce932d55fde1204034700b3a6551e30de1
owner <munimali250@gmail.com>
2023-09-27 20:13:39 -0400

    More Methods are being worked on, and more check marks have been ticked

--- src/BillSaver.java
+++ src/BillSaver.java
@@ -3,23 +3,32 @@ public class BillSaver {
     private int billNumber;
     private String itemsOrdered;
     private int totalItems;
-    private double amountToBePayedNoTip;
+    private double billPrice;
     private double getAmountToBePayedTip;
-    private double tipPercent;
+    private double tip;
     private int groupSize;
 
-    public BillSaver (int bN, int gS, int tI, String iO, double aTBPNT){
+    public BillSaver (int bN, int gS, int tI, String iO, double bP){
         billNumber = bN;
         groupSize = gS;
         totalItems = tI;
         itemsOrdered = iO;
-        amountToBePayedNoTip = aTBPNT;
+        billPrice = (Math.round(100 * bP)) / 100.0;
     }
-
+    public double tipAmount(double t) {
+        tip = t;
+        return billPrice / tip;
+    }
     public void billBefore() {
         System.out.println ("This is bill number " + billNumber);
         System.out.println("-- Items Ordered Today --");
         System.out.println(itemsOrdered);
-        System.out.println("The Total Price Before Tip is: " + amountToBePayedNoTip);
+        System.out.println("The per person cost before tip is: " + (billPrice / groupSize));
+        System.out.println("The Total Price Before Tip is: " + billPrice);
+
+    }
+
+    public boolean yesOrNoTip(String decision) {
+        return decision.equals("yes");
     }
 }
--- src/TipCalc.java
+++ src/TipCalc.java
@@ -11,6 +11,8 @@ public class TipCalc {
         String itemAddedToBill = "empty";
         int togetherPay;
         double billTotalPrice = 0;
+        String tipDecision;
+        double tip;
 
 
         // Group Size & If Paying Together
@@ -18,9 +20,11 @@ public class TipCalc {
         System.out.print("How many people are in your group: ");
         groupSize = scan.nextInt();
         scan.nextLine();
+
         System.out.print("Type 1 to pay together, Type 0 to pay separately: ");
         togetherPay = scan.nextInt();
         scan.nextLine();
+
         while (togetherPay != 1 && togetherPay != 0) {
             System.out.print("Please type 1 or 0, not any other number: ");
             togetherPay = scan.nextInt();
@@ -30,6 +34,8 @@ public class TipCalc {
         // checking how many bills to create
         if (togetherPay == 1) {
             System.out.println("Okay! Only one bill will be printed.");
+
+            // repeats until customer puts in all items
             while (!itemAddedToBill.equals("done")) {
                 System.out.print("Enter an item bought today, or done to finish: ");
                 itemAddedToBill = scan.nextLine();
@@ -43,36 +49,53 @@ public class TipCalc {
                         billTotalPrice += priceAddedToBill;
                     }
             }
+
             BillSaver single = new BillSaver(1 , groupSize, itemsOrdered, bill, billTotalPrice);
             single.billBefore();
+            System.out.print("Type 'Yes' if would you like to tip: ");
+            tipDecision = scan.nextLine();
+            if (single.yesOrNoTip(tipDecision)) {
+                System.out.print("Thank you! How much would you like to tip?: ");
+
+            } else {
+                System.out.println("Thank you for enjoying a meal at Jade's!");
+            }
+
         }
         else {
             System.out.print("One, Two, or Three separate bills?: ");
             int numberOfBills = scan.nextInt();
             scan.nextLine();
             while (numberOfBills != 1 && numberOfBills !=2 && numberOfBills != 3) {
+                for (int i = 1; i <= numberOfBills; ++i) {
+                    bill += "\nBill " + i + "\n";
+                    System.out.println("This is Bill " + i + "\n");
 
-            }
-            for (int i = 1; i <= numberOfBills; ++i) {
-                bill += "\nBill " + i + "\n";
-                System.out.println("This is Bill " + i + "\n");
-                while (!itemAddedToBill.equals("done")) {
-                    System.out.print("Enter an item bought today, or done to finish: ");
-                    itemAddedToBill = scan.nextLine();
-                    if (!itemAddedToBill.equals("done")) {
-                        System.out.print("Type the price of the item: ");
-                        priceAddedToBill = scan.nextDouble();
-                        scan.nextLine();
-                        billPerPerson += priceAddedToBill;
-                        bill += itemAddedToBill + " - $" + priceAddedToBill + "\n";
+                    // repeats until customer puts in all items
+                    while (!itemAddedToBill.equals("done")) {
+                        System.out.print("Enter an item bought today, or done to finish: ");
+                        itemAddedToBill = scan.nextLine();
+                        if (!itemAddedToBill.equals("done")) {
+                            System.out.print("Type the price of the item: ");
+                            priceAddedToBill = scan.nextDouble();
+                            billPerPerson = priceAddedToBill;
+                            scan.nextLine();
+                            itemsOrdered++;
+                            bill += itemAddedToBill + " - $" + priceAddedToBill + "\n";
+                            billTotalPrice += priceAddedToBill;
+                        }
+                    }
+
+                    // checking which bill to save the orders to
+                    if (i == 1) {
+                        BillSaver billOne = new BillSaver(i, groupSize, itemsOrdered, bill, billTotalPrice);
+                    } else if (i == 2) {
+                        BillSaver billTwo = new BillSaver(i, groupSize, itemsOrdered, bill, billTotalPrice);
+                    } else {
+                        BillSaver billThree = new BillSaver(i, groupSize, itemsOrdered, bill, billTotalPrice);
                     }
                 }
-                itemAddedToBill = "empty";
-                bill += "The bill before tips is $" + billPerPerson + "\n";
-                billPerPerson = 0;
             }
         }
-
-
     }
 }

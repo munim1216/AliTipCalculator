82789bbab589eb3d3d56fd820731a31b36fee298
owner <munimali250@gmail.com>
2023-09-29 08:33:06 -0400

    shouldn't have any major bugs left, and should be pretty much done

--- src/Check.java
+++ src/Check.java
@@ -1,7 +1,11 @@
 public class Check {
     public Check() {}
     public String lastDigit(double check) {
-        if (((check * 100) % 10) == 0){
+        /* code to ask mr miller about
+        System.out.println(check * 100);
+        System.out.println((check * 100) % 10);
+        */
+        if ((Math.round(check * 100) % 10) == 0){
             if ((((int)(check * 10)) / 10) == check) {
                 return "";
             } else {
